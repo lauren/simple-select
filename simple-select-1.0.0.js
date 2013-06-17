b4ca9a12d2aaedaaf9693773feeb14a99eeedd9e
lauren <lauren.sperber@gmail.com>
2013-06-17 12:58:29 -0400

    simplify parser function and change terminology of 'parents' to 'ancestors'

--- simple-select-1.0.0.js
+++ simple-select-1.0.0.js
@@ -14,52 +14,29 @@
   var parseSelectors = function (selectors) {
     selectors = selectors.split(" ");
     // change each selector into object with ID/class/node property based
-    // on first character
+    // on first character, then parse out additional classes and IDs in 
+    // each property in the new object if applicable.
     // we're going from ["node.class", "node#id", "#id.class.class", ".class.class"] to
-    // [{node: 'node.class'}, {node: 'node#id'}, {id: #id.class.class}, {classes: ["class.class"]}]
+    // [{node: 'node', classes: ['.class'}, {node: 'node', id: '#id'}, 
+    // {id: '#id', classes: ['.class', '.class']}, {classes: ['class', '.class']}]
     for (var i = 0; i < selectors.length; i++) {
       switch(selectors[i].charAt(0)) {
-      case "#":
+        case "#":
           selectors[i] = {id: selectors[i]};
+          findClassesInObject(selectors[i]);
           break;
-      case ".":
-          selectors[i] = {classes: [selectors[i]]};
+        case ".":
+          selectors[i] = {classes: findClasses(selectors[i])};
           break;
-      default:
+        default:
           selectors[i] = {node: selectors[i]};
+          if (/#/.test(selectors[i].node)) {
+            selectors[i] = findNodeIds(selectors[i].node);
+          }
+          findClassesInObject(selectors[i]);
       }
     };
-      
-    // find additional classes and IDs in the main selector to flush out the object
-    // we're going from 
-    // [{node: 'node.class'}, {node: 'node#id'}, {id: #id.class.class}, {classes: ["class.class"]}] to
-    // [{node: "node", classes: [".class"]}, {node: "node", id: "#id"}, 
-    // {id: "#id", classes: [".class", ".class"]}, {classes: [".class", ".class"]}]
-    // TODO: make an object with all the functions that happen for each case, then call it from another function like
-    // var handler = function (stuff) {
-    // return handlers[thing]();
-    //}
-    // use map instead of looping var result = [0,1,2].map(function () {}) ? won't work in IE6 :(
-    for (var i = 0; i < selectors.length; i++) {
-      if (selectors[i].classes) {
-        selectors[i].classes = findClasses(selectors[i].classes[0]);
-      }
-      if (selectors[i].node) {
-        if (/#/.test(selectors[i].node)) {
-          selectors[i] = findNodeIds(selectors[i].node);
-        }
-        if (/\./.test(selectors[i].node)) {
-          selectors[i].classes = findClasses(selectors[i].node);
-          selectors[i].node = selectors[i].node.split(".")[0];
-        }
-      }
-      if (selectors[i].id) {
-        if (/\./.test(selectors[i].id)) {
-          selectors[i].classes = findClasses(selectors[i].id);
-          selectors[i].id = selectors[i].id.split(".")[0];
-        }
-      }
-    };
+  
     return selectors;
   }
   
@@ -81,28 +58,39 @@
     }
     return classes;
   };
+
+  // takes a selector object like {node: "node.class.class"} or {id: "#id.class.class"} or 
+  // {node: "node"} or {id: "#id"} and returns {node: "node", classes: [".class", ".class"]} or 
+  // {id: "#id", classes: [".class", ".class"]} or {node: "node"} or {id: "#id"}
+  var findClassesInObject = function (object) {
+    for (type in object) {
+      if (/\./.test(object[type])) {
+        object.classes = findClasses(object[type]);
+        object[type] = object[type].split(".")[0];
+      }
+    }
+  };
   
   // iterates over array of elements and checks them against provided function
   // with provided array of selectors. if result === true, push to matchingElements
   var checkAndPushElements = function (elements,checkingFunction,selectors) {
     var matchingElements = [];
     for (var i = 0; i < elements.length; i++) {   
-      if (checkingFunction(elements[i],findAllParents(elements[i]),selectors)) {
+      if (checkingFunction(elements[i],findAncestors(elements[i]),selectors)) {
         matchingElements.push(elements[i]);
       }
     };
     return matchingElements;
   };
   
-  var findAllParents = function (thisElement) {
-    var parents = [];
+  var findAncestors = function (thisElement) {
+    var ancestors = [];
     while (thisElement.parentNode) {
-      parents.unshift(thisElement.parentNode);
+      ancestors.unshift(thisElement.parentNode);
       thisElement = thisElement.parentNode;
     }
-    return parents;
+    return ancestors;
   };
-  
   
   var matchOneElementToOneSelector = function (element,selector) {
     var elementNode = element.nodeName.toLowerCase(),
@@ -124,11 +112,11 @@
     return result;
   };
   
-  var matchSelectorToParents = function (theseParents,thisSelector) {
-    var closestParent = theseParents.slice(0).pop(),
+  var matchSelectorToParents = function (theseAncestors,thisSelector) {
+    var closestParent = theseAncestors.slice(0).pop(),
       currentMatch
     result = {match: false, matchingElement: false};
-    if (theseParents.length <= 1) {
+    if (theseAncestors.length <= 1) {
       result.match = false;
       return result;
     } else {
@@ -137,14 +125,14 @@
         result.matchingElement = closestParent;
         return result;
       } else {
-        matchSelectorToParents(theseParents.slice(0,theseParents.length-1),thisSelector);
+        matchSelectorToParents(theseAncestors.slice(0,theseAncestors.length-1),thisSelector);
       }
     }
     return result;
   };
   
-  //change theseParents to ancestors
-  var matchToSelectors = function (thisElement,theseParents,theseSelectors) {
+  //change theseAncestors to ancestors
+  var matchToSelectors = function (thisElement,theseAncestors,theseSelectors) {
     var selectorCount = theseSelectors.length,
         lastSelector = theseSelectors[selectorCount-1],
         penultimateSelector = theseSelectors[selectorCount-2],
@@ -155,10 +143,10 @@
     } else if (selectorCount === 1) {
       return matchOneElementToOneSelector(thisElement,theseSelectors[0]);
     } else {
-      if (matchOneElementToOneSelector(thisElement,lastSelector) && theseParents.length > 1) {
-        nextMatch = matchSelectorToParents(theseParents,penultimateSelector);
+      if (matchOneElementToOneSelector(thisElement,lastSelector) && theseAncestors.length > 1) {
+        nextMatch = matchSelectorToParents(theseAncestors,penultimateSelector);
         if (nextMatch.match) {
-          nextMatchParents = findAllParents(nextMatch.matchingElement);
+          nextMatchParents = findAncestors(nextMatch.matchingElement);
           return (theseSelectors.length === 2) ? true : matchToSelectors(nextMatch.matchingElement,nextMatchParents,
             theseSelectors.slice(0,theseSelectors.length-1));
         } else {
